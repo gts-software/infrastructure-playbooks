8bc24fbe47fbb6c192ab0bbda63946a3d209ec31
Niklas Salmoukas <niklas@salmoukas.com>
2017-02-16 03:33:34 +0100

    improve inventory scripts

--- inventory-add.sh
+++ inventory-add.sh
@@ -1,15 +1,32 @@
 #!/bin/bash
 set -e
 
+# retrieve parameter
+
 HOST_NAME="$1"
 HOST_IP_DNS="$2"
 ROOT_PASSWORD_CURRENT="$3"
-ROOT_PASSWORD_NEW="`< /dev/urandom tr -dc A-Za-z0-9 | head -c64; echo`"
+ROOT_PASSWORD_NEW="$4"
+
+if [ -z "$ROOT_PASSWORD_NEW" ]
+then
+  ROOT_PASSWORD_NEW="$ROOT_PASSWORD_CURRENT"
+fi
+
+if [ "$ROOT_PASSWORD_NEW" == "auto" ]
+then
+  ROOT_PASSWORD_NEW="`< /dev/urandom tr -dc A-Za-z0-9 | head -c64; echo`"
+fi
+
 INVENTORY_DIRECTORY="`pwd`"
+
+# validate parameter
 
 if [ -z "$HOST_NAME" ] || [ -z "$HOST_IP_DNS" ] || [ -z "$ROOT_PASSWORD_CURRENT" ];
 then
-  echo "Usage: $0 <host name> <host ip or dns> <current root password>"
+  echo "Usage: $0 <host name> <host ip or dns> <current root password> [<new root password>|auto]"
+  echo "- If you provide a new root password, we will change the password on the host automatically."
+  echo "- If you provide the keyword 'auto', we will generate a strong password automatically."
   exit 1
 fi
 
@@ -20,65 +37,38 @@ echo "  - current root password: $ROOT_PASSWORD_CURRENT"
 echo "  - new root password: $ROOT_PASSWORD_NEW"
 echo "  - inventory directory: $INVENTORY_DIRECTORY"
 
-# create basic structure of inventory directory if required
+# validate inventory structure
 
-echo "* validating inventory directory:"
-
-if [ ! -f "$INVENTORY_DIRECTORY/ansible.cfg" ];
-then
-  echo "  - creating ansible.cfg file"
-  echo -e "[defaults]\ninventory = hosts" > "$INVENTORY_DIRECTORY/ansible.cfg"
-else
-  echo "  - file ansible.cfg exists"
-fi
+echo "* validating inventory structure:"
 
 if [ ! -f "$INVENTORY_DIRECTORY/hosts" ];
 then
-  echo "  - creating empty hosts file"
-  touch "$INVENTORY_DIRECTORY/hosts"
+  echo "  - file hosts missing (error)"
+  echo "* ABORTING!"
+  exit 2
 else
-  echo "  - file hosts exists"
+  echo "  - file hosts exists (ok)"
 fi
 
 if [ ! -d "$INVENTORY_DIRECTORY/host_vars" ];
 then
-  echo "  - creating empty host_vars directory"
+  echo "  - creating empty host_vars directory (ok)"
   mkdir -p "$INVENTORY_DIRECTORY/host_vars"
 else
-  echo "  - directory host_vars exists"
+  echo "  - directory host_vars exists (ok)"
 fi
 
-if [ ! -d "$INVENTORY_DIRECTORY/group_vars" ];
-then
-  echo "  - creating empty group_vars directory"
-  mkdir -p "$INVENTORY_DIRECTORY/group_vars"
-else
-  echo "  - directory group_vars exists"
-fi
+# validate if host is new
 
-# validate if group_vars/all.yml is defined already
-
-echo "* validating if group_vars is populated:"
-
-if [ ! -f "$INVENTORY_DIRECTORY/group_vars/all.yml" ];
-then
-  echo "  - creating group_vars/all.yml file"
-  echo -e "# base\nbase_name_domain: example.local" > "$INVENTORY_DIRECTORY/group_vars/all.yml"
-else
-  echo "  - file group_vars/all.yml exists"
-fi
-
-# validate if host is not defined already
-
-echo "* validating if host is not defined already:"
+echo "* validating if host is new:"
 
 if grep -q "^$HOST_NAME\$" "$INVENTORY_DIRECTORY/hosts"
 then
-  echo "  - host $HOST_NAME is defined in hosts file already (error)"
+  echo "  - host $HOST_NAME is defined in hosts file (error)"
   echo "* ABORTING!"
   exit 2
 else
-  echo "  - host $HOST_NAME is is not defined in hosts file (ok)"
+  echo "  - host $HOST_NAME is not defined in hosts file (ok)"
 fi
 
 if [ -e "$INVENTORY_DIRECTORY/host_vars/$HOST_NAME" ] || [ -e "$INVENTORY_DIRECTORY/host_vars/$HOST_NAME.yml" ];
@@ -101,6 +91,7 @@ echo -e "# ansible\nansible_host: $HOST_IP_DNS\nansible_user: root\nansible_pass
 echo "  - added host_vars/$HOST_NAME.yml file"
 
 # update password on host
+
 echo "* updating password on host:"
 echo "root:$ROOT_PASSWORD_NEW" | sshpass -p "$ROOT_PASSWORD_CURRENT" ssh "root@$HOST_IP_DNS" chpasswd
 echo "DONE!"
