4add2c766808bb2caa9eededa6b837fbe8df8482
Niklas Salmoukas <niklas@salmoukas.com>
2017-02-19 07:40:40 +0100

    directory juggling

--- build-project.sh
+++ build-project.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
 
+OLD_DIR="`pwd`"
 PLAYBOOK_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 while getopts p:s:m:b:v: option
@@ -30,8 +31,8 @@ done
 
 ${PROJECT_SOURCE:="`pwd`"}
 ${PROJECT_MODE:='staging'}
-${PROJECT_BRANCH:="`git rev-parse --abbrev-ref HEAD 2> /dev/null || echo 'unknown'`"}
-${PROJECT_VERSION:="`git rev-parse --verify HEAD 2> /dev/null || echo 'unknown'`"}
+${PROJECT_BRANCH:="`cd "$PROJECT_SOURCE"; git rev-parse --abbrev-ref HEAD 2> /dev/null || echo 'unknown'`"}
+${PROJECT_VERSION:="`cd "$PROJECT_SOURCE"; git rev-parse --verify HEAD 2> /dev/null || echo 'unknown'`"}
 
 echo "Build:"
 echo "* PLAYBOOK_DIR=$PLAYBOOK_DIR"
@@ -40,5 +41,8 @@ echo "* PROJECT_MODE=$PROJECT_MODE"
 echo "* PROJECT_BRANCH=$PROJECT_BRANCH"
 echo "* PROJECT_VERSION=$PROJECT_VERSION"
 
+cd "$PLAYBOOK_DIR"
 ansible-playbook $PLAYBOOK_DIR/build-project.yml -e project_source=$PROJECT_SOURCE -e project_mode=$PROJECT_MODE -e project_branch=$PROJECT_BRANCH -e project_version=$PROJECT_VERSION -e @project.yml
+
+cd "$OLD_DIR"
 echo "Done!"
--- deploy-project.sh
+++ deploy-project.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
 
+OLD_DIR="`pwd`"
 PLAYBOOK_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 while getopts p:m:b:v: option
@@ -8,6 +9,9 @@ do
   case "${option}" in
     p)
       PLAYBOOK_DIR="$OPTARG"
+      ;;
+    s)
+      PROJECT_SOURCE="$OPTARG"
       ;;
     m)
       PROJECT_MODE="$OPTARG"
@@ -19,15 +23,16 @@ do
       PROJECT_VERSION="$OPTARG"
       ;;
     \?)
-      echo "Usage: $0 [-p playbook_dir] [-m project_mode] [-p project_branch] [-v project_version]"
+      echo "Usage: $0 [-p playbook_dir] [-s project_source] [-m project_mode] [-p project_branch] [-v project_version]"
       exit 1
       ;;
   esac
 done
 
+${PROJECT_SOURCE:="`pwd`"}
 ${PROJECT_MODE:='staging'}
-${PROJECT_BRANCH:="`git rev-parse --abbrev-ref HEAD 2> /dev/null || echo 'unknown'`"}
-${PROJECT_VERSION:="`git rev-parse --verify HEAD 2> /dev/null || echo 'unknown'`"}
+${PROJECT_BRANCH:="`cd "$PROJECT_SOURCE"; git rev-parse --abbrev-ref HEAD 2> /dev/null || echo 'unknown'`"}
+${PROJECT_VERSION:="`cd "$PROJECT_SOURCE"; git rev-parse --verify HEAD 2> /dev/null || echo 'unknown'`"}
 
 echo "Deploy:"
 echo "* PLAYBOOK_DIR=$PLAYBOOK_DIR"
@@ -35,5 +40,8 @@ echo "* PROJECT_MODE=$PROJECT_MODE"
 echo "* PROJECT_BRANCH=$PROJECT_BRANCH"
 echo "* PROJECT_VERSION=$PROJECT_VERSION"
 
+cd "$PLAYBOOK_DIR"
 ansible-playbook $PLAYBOOK_DIR/deploy-project.yml -e project_mode=$PROJECT_MODE -e project_branch=$PROJECT_BRANCH -e project_version=$PROJECT_VERSION -e @project.yml
+
+cd "$OLD_DIR"
 echo "Done!"
