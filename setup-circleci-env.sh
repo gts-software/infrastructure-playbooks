eaafad183300071ba6f75df15e6320977a6c6fe6
Niklas Salmoukas <niklas@salmoukas.com>
2017-02-19 07:36:15 +0100

    more output

--- setup-circleci-env.sh
+++ setup-circleci-env.sh
@@ -3,15 +3,18 @@ set -e
 cd "$HOME"
 
 # setup docker
+echo "Setting up Docker..."
 curl -fsSLO https://get.docker.com/builds/Linux/x86_64/docker-1.13.1.tgz
 sudo tar --strip-components=1 -xvzf docker-1.13.1.tgz -C /usr/local/bin
 sudo container=yes docker daemon: { background: true }
 
 # setup ansible
+echo "Setting up Ansible..."
 sudo pip install --upgrade pip
 sudo pip install ansible docker-py toposort
 
 # setup environment
+echo "Setting up Environment..."
 if [[ "${GIT_BRANCH}" == "production" ]];
 then
   echo "export PROJECT_MODE=production" >> .circlerc
@@ -20,3 +23,5 @@ else
 fi
 echo "export PROJECT_BRANCH=$CIRCLE_BRANCH" >> .circlerc
 echo "export PROJECT_VERSION=$CIRCLE_SHA1" >> .circlerc
+
+echo "Done!"
