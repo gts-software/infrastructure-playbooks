07d9ffad60b7b421c801e6be880bf773aa3a2751
Niklas Salmoukas <niklas@salmoukas.com>
2017-09-02 02:24:07 +0200

    improve exit codes, generate joblog

--- roles/backup-service/files/backup-object-main.sh
+++ roles/backup-service/files/backup-object-main.sh
@@ -9,30 +9,38 @@ BACKUP_OBJECT="$1"
 
 # make sure object directory exist
 echo ">> INFO: running backup for $BACKUP_OBJECT"
-mkdir -p "/backup/repos/$BACKUP_OBJECT"
+if ! mkdir -p "/backup/repos/$BACKUP_OBJECT";
+then
+  exit 1
+fi
+
+#### backup phase
 
 # run rdiff-backup
 echo ">> INFO: running rdiff-backup for $BACKUP_OBJECT"
 if ! /backup/scripts/backup-object-rdiff-backup.sh "$BACKUP_OBJECT";
 then
-  EXIT_CODE="$?"
   echo ">> ERROR: rdiff-backup failed for $BACKUP_OBJECT"
-  exit $EXIT_CODE
+  exit 2
 fi
 
 # run duplicity
 echo ">> INFO: running duplicity for $BACKUP_OBJECT"
 if ! /backup/scripts/backup-object-duplicity.sh "$BACKUP_OBJECT";
 then
-  EXIT_CODE="$?"
   echo ">> ERROR: duplicity failed for $BACKUP_OBJECT"
-  exit $EXIT_CODE
+  exit 3
 fi
+
+#### cleanup phase
+
+EXIT_CODE="0"
 
 # cleanup rdiff-backup
 echo ">> INFO: cleanup rdiff-backup for $BACKUP_OBJECT"
 if ! rdiff-backup --remove-older-than "$REMOVE_OLDER_THAN" --force "/backup/repos/$BACKUP_OBJECT";
 then
+  EXIT_CODE="4"
   echo ">> ERROR: cleanup rdiff-backup failed for $BACKUP_OBJECT"
 fi
 
@@ -44,8 +52,10 @@ export AWS_SECRET_ACCESS_KEY="$AWS_KEY_SECRET"
 
 if ! duplicity remove-older-than "$REMOVE_OLDER_THAN" --force "$AWS_S3_URL/$BACKUP_OBJECT";
 then
+  EXIT_CODE="5"
   echo ">> ERROR: cleanup duplicity failed for $BACKUP_OBJECT"
 fi
 
 # done
 echo ">> INFO: completed backup for $BACKUP_OBJECT"
+exit "$EXIT_CODE"
--- roles/backup-service/files/backup.sh
+++ roles/backup-service/files/backup.sh
@@ -2,8 +2,8 @@
 set -e
 
 # run up to 3 backups in parallel
-NOW=$(date +%Y%m%d-%H%M%S)
-parallel --will-cite --jobs 3 /backup/scripts/backup-object.sh {} '&>' /backup/logs/{}-$NOW.log < /backup/config/objects.list
+export BACKUP_TIMESTAMP=$(date +%Y%m%d-%H%M%S)
+parallel --will-cite --keep-order --jobs 3 --joblog /backup/logs/joblog-$BACKUP_TIMESTAMP.log /backup/scripts/backup-object.sh {} '&>' /backup/logs/{}-$BACKUP_TIMESTAMP.log < /backup/config/objects.list
 
 # cleanup logfiles
 find /backup/logs/ -type f -mtime +14 -delete
