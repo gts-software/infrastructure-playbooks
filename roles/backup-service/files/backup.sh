084622f4a626a999e478a3183fc5e8198e6d3e82
Niklas Salmoukas <niklas@salmoukas.com>
2017-09-02 03:22:18 +0200

    log output in case backup job failed

--- roles/backup-service/files/backup.sh
+++ roles/backup-service/files/backup.sh
@@ -3,7 +3,23 @@ set -e
 
 # run up to 3 backups in parallel
 export BACKUP_TIMESTAMP=$(date +%Y%m%d-%H%M%S)
-parallel --will-cite --keep-order --jobs 3 --joblog /backup/logs/joblog-$BACKUP_TIMESTAMP.log /backup/scripts/backup-object.sh {} '&>' /backup/logs/{}-$BACKUP_TIMESTAMP.log < /backup/config/objects.list
+if ! parallel --will-cite --keep-order --jobs 3 --joblog /backup/logs/joblog-$BACKUP_TIMESTAMP.log /backup/scripts/backup-object.sh {} '&>' /backup/logs/{}-$BACKUP_TIMESTAMP.log < /backup/config/objects.list;
+then
+  # collect number of backup jobs failed
+  JOBS_FAILED_COUNT=$?
+
+  # print warning
+  echo "BACKUP FAILED: $JOBS_FAILED_COUNT backup(s) could not be completed!"
+  echo
+
+  # print logs
+  for LOGFILE in `ls /backup/logs/*-$BACKUP_TIMESTAMP.log | sort`;
+  do
+    echo "################ `basename $LOGFILE` ################"
+    echo "`cat $LOGFILE`"
+    echo
+  done
+fi
 
 # cleanup logfiles
 find /backup/logs/ -type f -mtime +14 -delete
