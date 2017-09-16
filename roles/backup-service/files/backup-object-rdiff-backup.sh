268be9e996e2ceab31cf7b399316343f597290f6
Niklas Salmoukas <niklas@salmoukas.com>
2017-09-16 22:07:46 +0200

    do not use ssh compression with rdiff-backup to improve speed

--- roles/backup-service/files/backup-object-rdiff-backup.sh
+++ roles/backup-service/files/backup-object-rdiff-backup.sh
@@ -4,7 +4,7 @@
 BACKUP_OBJECT="$1"
 
 # run rdiff-backup
-rdiff-backup --print-statistics --exclude-device-files --exclude-fifos --exclude-sockets --preserve-numerical-ids "root@$BACKUP_OBJECT::/mnt/root-snapshot" "/backup/repos/$BACKUP_OBJECT"
+rdiff-backup --ssh-no-compression --print-statistics --exclude-device-files --exclude-fifos --exclude-sockets --preserve-numerical-ids "root@$BACKUP_OBJECT::/mnt/root-snapshot" "/backup/repos/$BACKUP_OBJECT"
 EXIT_CODE="$?"
 
 # add some sleep to allow all output to be captured
