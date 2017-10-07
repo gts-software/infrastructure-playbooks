049097beb3c111027fda09a863b3263b828cabca
Niklas Salmoukas <niklas@hofmann.io>
2017-10-07 23:02:29 +0200

    remove email param from docker login

--- circleci/setup-environment.sh
+++ circleci/setup-environment.sh
@@ -14,7 +14,7 @@ echo "export PROJECT_VERSION=$CIRCLE_SHA1" >> "$BASH_ENV"
 
 # docker login
 echo "Perform docker login..."
-docker login --username ${QUAY_USER} --email ${QUAY_MAIL} --password ${QUAY_PASS} quay.io
+docker login --username ${QUAY_USER} --password ${QUAY_PASS} quay.io
 
 # ssh known_hosts
 echo "Setup SSH known hosts..."
