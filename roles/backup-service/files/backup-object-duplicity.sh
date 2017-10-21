b02868bd3eb30c9bb350c0170b1b6243b70142b7
Niklas Salmoukas <niklas@hofmann.io>
2017-10-21 16:58:56 +0200

    add --allow-source-mismatch flag to duplicity to allow host name changes

--- roles/backup-service/files/backup-object-duplicity.sh
+++ roles/backup-service/files/backup-object-duplicity.sh
@@ -9,7 +9,7 @@ BACKUP_OBJECT="$1"
 # run rdiff-backup
 export AWS_ACCESS_KEY_ID="$CONFIG_AWS_KEY_ID"
 export AWS_SECRET_ACCESS_KEY="$CONFIG_AWS_KEY_SECRET"
-duplicity --max-blocksize 4194304 --asynchronous-upload --no-encryption --full-if-older-than "$CONFIG_REMOTE_FULL_IF_OLDER_THAN" --exclude "/backup/repos/$BACKUP_OBJECT/rdiff-backup-data" "/backup/repos/$BACKUP_OBJECT" "$CONFIG_AWS_S3_URL/$BACKUP_OBJECT"
+duplicity --allow-source-mismatch --max-blocksize 4194304 --asynchronous-upload --no-encryption --full-if-older-than "$CONFIG_REMOTE_FULL_IF_OLDER_THAN" --exclude "/backup/repos/$BACKUP_OBJECT/rdiff-backup-data" "/backup/repos/$BACKUP_OBJECT" "$CONFIG_AWS_S3_URL/$BACKUP_OBJECT"
 EXIT_CODE="$?"
 
 # done!
